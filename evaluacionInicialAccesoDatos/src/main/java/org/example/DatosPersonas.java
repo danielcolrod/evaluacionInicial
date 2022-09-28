567cd73e080c59f06246c7769beaf1bf56add96f
danielcolrod <dcolrod0604@g.educaand.es>
2022-09-28 12:06:04 +0200

    empezando generador de personas

---
+++ evaluacionInicialAccesoDatos/src/main/java/org/example/DatosPersonas.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DatosPersonas {
+
+    public static final String[] listaNombre = new String[] {
+            "Daniel", "Pablo", "Alberto", "Pepe", "Rubén", "Mario", "Nicolas"
+    };
+
+    public static final String[] listaApellidos = new String[] {
+            "Colmenero Rodriguez", "Ruiz Perez", "Martinez Carrasco", "García Marín", "Martínez Moreno", "Reyes Lendínez", "Molina Castro"
+    };
+
+    public static final String[] listaDirecciones = new String[] {
+            "Avenida Andalucía, Nº20, 2ºA", "Calle Pintor Ribera, Nº12", "Alberto", "Pepe", "Rubén", "Mario", "Nicolas"
+    };
+}
---
+++ evaluacionInicialAccesoDatos/src/main/java/org/example/Persona.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class Persona {
+
+    private String nombre;
+    private String apellidos;
+    private String dni;
+    private String email;
+    private String ciudadCP;
+
+    public Persona(String nombre, String apellidos, String dni, String email, String ciudadCP) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.dni = dni;
+        this.email = email;
+        this.ciudadCP = ciudadCP;
+    }
+
+    public Persona() {
+    }
+}
